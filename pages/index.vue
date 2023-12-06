89c8d1a09b1fc3f6193170125e23a01a9095b3fa
ydehoj <148390464+ydehoj@users.noreply.github.com>
2023-12-06 15:33:46 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,22 +3,10 @@
     <div class="intro">
       <h1>Nuxt Toolbox</h1>
       <p>
-        Hi 👋! This template gives you a
-        <a href="https://nuxtjs.org/">Nuxt</a> app with the scaffolding for
-        <a href="https://www.netlify.com/products/functions/"
-          >Netlify Functions</a
-        >, <a href="https://www.netlify.com/products/forms/">Forms</a>, and
-        <a href="https://docs.netlify.com/routing/redirects/">Redirects</a>. Our
-        aim was to give you the code you would need to hit the ground running
-        with a few fun features.
+        Wil jij voor de eerste keer de studio boeken? Meld je dan eerst aan via ons registratieformulier.
+        Wij nemen hierna binnen 72 uur contact met jou op om de mogelijkheden te bespreken.
       </p>
 
-      <p>
-        You can find the code for this project on GitHub at
-        <a href="https://github.com/netlify-templates/nuxt-toolbox"
-          >https://github.com/netlify-templates/nuxt-toolbox</a
-        >! Happy coding!
-      </p>
     </div>
     <FeedbackForm />
     <JokeBlock />
